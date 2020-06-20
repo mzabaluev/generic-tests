8998f7c200bb2b265b19094515d146fe885b3c9a
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2020-06-21 00:43:01 +0300

    Add a test, requiring an unstable feature
    In the current usage, submodules for instantiation are given
    in the external form, which is only possible with the
    proc_macro_hygiene feature.

---
+++ tests/tests.rs
@@ -0,0 +1,23 @@
+#![feature(proc_macro_hygiene)]
+
+#[generic_tests::module]
+mod tests {
+    use std::borrow::Cow;
+    use std::fmt::Debug;
+
+    #[test]
+    fn equates_to_str<S: From<&'static str>>()
+    where
+        S: ?Sized + PartialEq<str> + Debug,
+    {
+        let s: S = "ab".into();
+        assert_eq!(&s, "ab");
+        assert_ne!(&s, "aa");
+    }
+
+    #[instantiate_tests(String)]
+    mod string;
+
+    #[instantiate_tests(Cow<'static, str>)]
+    mod cow;
+}
