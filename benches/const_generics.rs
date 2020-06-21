e6bab9214fce87c10f60ed16a60e27662d2d1602
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2020-06-21 22:51:51 +0300

    Add a benchmark that uses const generics
    Verify that benchmarks work and, now that we are into unstable features,
    demonstrate test parameterization with const generics.

---
+++ benches/const_generics.rs
@@ -0,0 +1,25 @@
+#![allow(incomplete_features)]
+#![feature(const_generics)]
+#![feature(test)]
+
+extern crate test;
+
+#[generic_tests::define]
+mod benches {
+    use std::iter;
+    use test::Bencher;
+
+    #[bench]
+    fn fill_vec<const LEN: usize>(b: &mut Bencher) {
+        b.iter(|| {
+            let v: Vec<u8> = iter::repeat(0xA5).take(LEN).collect();
+            test::black_box(v);
+        })
+    }
+
+    #[instantiate_tests(<16>)]
+    mod small {}
+
+    #[instantiate_tests(<65536>)]
+    mod large {}
+}
