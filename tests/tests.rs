14e16ada3d0ee612b3f1e3fe723fe4926a533f01
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2020-06-21 12:08:42 +0300

    Test use in nested submodules
    Exercise the case when the instantiation module's depth is
    greater than 1.

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,5 +1,5 @@
 #[generic_tests::define]
-mod tests {
+mod simple {
     use std::borrow::Cow;
     use std::fmt::Debug;
 
@@ -18,4 +18,59 @@ mod tests {
 
     #[instantiate_tests(<Cow<'static, str>>)]
     mod cow {}
+}
+
+#[generic_tests::define]
+mod nested {
+    use std::fmt::{self, Display};
+
+    #[test]
+    fn print<T: Display + Default>() {
+        let v = T::default();
+        println!("{}", v);
+    }
+
+    #[derive(Default)]
+    struct Foo;
+
+    impl Display for Foo {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "Foo")
+        }
+    }
+
+    mod from_prelude {
+        #[instantiate_tests(<String>)]
+        mod string {}
+    }
+
+    mod imported {
+        use std::borrow::Cow;
+
+        #[instantiate_tests(<Cow<'static, str>>)]
+        mod cow {}
+    }
+
+    mod locally_defined {
+        use std::fmt::{self, Display};
+
+        #[derive(Default)]
+        struct Bar;
+
+        impl Display for Bar {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "Bar")
+            }
+        }
+
+        #[instantiate_tests(<Bar>)]
+        mod bar {}
+    }
+
+    mod aliases_root {
+        use super::Foo;
+
+        #[instantiate_tests(<Foo>)]
+        mod foo {}
+    }
 }
